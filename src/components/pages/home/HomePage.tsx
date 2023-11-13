19edcde621da3ae7d11f77eaaaef172cdaec2995
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2023-11-13 22:35:38 +0700

    Feat: create homepage

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,11 +1,15 @@
 import * as S from "./HomePage.styles";
+import Profile from "@/assets/images/cat-profile.jpeg";
 
 import {
   Box,
+  Divider,
   Flex,
+  Image,
   HStack,
   Stack,
   StackDirection,
+  Text,
   VStack,
 } from "@chakra-ui/react";
 import { FC, HTMLAttributes } from "react";
@@ -23,30 +27,62 @@ export const HomePage: FC<HomePageProps> = ({
   children,
   ...props
 }) => {
-  const { isMobile } = useDevice();
+  const { isMobile, isTablet } = useDevice();
 
-  const customStyles: CustomStyleType = isMobile
-    ? {
-        direction: "column",
-      }
-    : {
-        direction: "row",
-        alignItems: "center",
-      };
+  const customStyles: CustomStyleType =
+    isMobile || isTablet
+      ? {
+          direction: "column-reverse",
+        }
+      : {
+          direction: "row",
+          alignItems: "center",
+        };
   return (
     <S.Wrapper id="home" className={className} {...props}>
       <Stack
         margin={"0 16px"}
         height={"100%"}
+        gap={10}
         position="relative"
         {...customStyles}
       >
         <VStack flex={1} alignItems={"flex-start"}>
-          <Box>Nghi Nguyen</Box>
-          <Box>Software Engineer</Box>
-          <Box>Description</Box>
+          <Text
+            color="default.title"
+            fontSize={isMobile ? "4xl" : "6xl"}
+            fontWeight="bold"
+            whiteSpace="nowrap"
+          >
+            Nghi Nguyen
+          </Text>
+
+          <Stack direction="row" width="100%" alignItems="center" gap={5}>
+            <Divider borderBottomWidth={2} width="50px" />
+            <Text
+              color="default.titleDark"
+              fontSize={isMobile ? "xl" : "2xl"}
+              whiteSpace="nowrap"
+            >
+              Frontend Software Engineer
+            </Text>
+          </Stack>
+          <Text color="default.text" fontSize={isMobile ? "sm" : "md"}>
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.
+          </Text>
         </VStack>
-        <Box flex={1}>image</Box>
+
+        <VStack flex={1}>
+          <Image
+            borderRadius="full"
+            boxSize={250}
+            src={Profile}
+            alt="My Profile"
+            margin="auto"
+            objectFit="cover"
+          />
+        </VStack>
       </Stack>
     </S.Wrapper>
   );
--- src/hooks/device.ts
+++ src/hooks/device.ts
@@ -1,7 +1,16 @@
 import { Hooks } from "minimist-react-library";
+import { useEffect, useState } from "react";
 
 export const useDevice = () => {
-  const isMobile = Hooks.Device.useResponsive({ maxWidth: 500 });
+  const [isMobile, setIsMobile] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
 
-  return { isMobile };
+  const { width } = Hooks.Device.useWindowSize();
+
+  useEffect(() => {
+    setIsMobile(width <= 425);
+    setIsTablet(width > 425 && width <= 768);
+  }, [width]);
+
+  return { isMobile, isTablet };
 };
