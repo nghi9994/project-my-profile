8d568b8c33e42b992c18fe8f9b22ea7f3a428409
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2023-11-14 23:37:55 +0700

    Feat: add react-icons lib

--- src/components/pages/about/AboutPage.tsx
+++ src/components/pages/about/AboutPage.tsx
@@ -1,8 +1,10 @@
 import * as S from "./AboutPage.styles";
 
+import { PROFILE_URL } from "@/constants";
 import { useDevice } from "@/hooks";
 import {
   Box,
+  Button,
   Image,
   Stack,
   StackDirection,
@@ -10,7 +12,8 @@ import {
   TypographyProps,
 } from "@chakra-ui/react";
 import { FC, HTMLAttributes } from "react";
-import { PROFILE_URL } from "@/constants";
+import { PiClipboardTextBold, PiMedal, PiConfetti } from "react-icons/pi";
+import { CgFileDocument } from "react-icons/cg";
 
 interface AboutPageProps extends HTMLAttributes<HTMLDivElement> {}
 type CustomStyleType = {
@@ -37,40 +40,119 @@ export const AboutPage: FC<AboutPageProps> = ({
         }
       : {
           direction: "row",
-          alignItems: "center",
+          alignItems: "flex-start",
           justifyContent: "space-around",
           textAlign: "left",
         };
 
   return (
     <S.Wrapper id="about" className={className} textAlign="center" {...props}>
-      <S.PageTitle color={"default.titleDark"} fontSize="4xl" fontWeight="bold">
-        About Me
-      </S.PageTitle>
-      <S.PageSubtitle color={"default.title"} fontSize="sm">
-        My Introduction
-      </S.PageSubtitle>
+      <Stack height="100%">
+        <S.PageTitle
+          color={"default.titleDark"}
+          fontSize="4xl"
+          fontWeight="bold"
+        >
+          About Me
+        </S.PageTitle>
+        <S.PageSubtitle color={"default.title"} fontSize="sm">
+          My Introduction
+        </S.PageSubtitle>
 
-      <Stack gap={6} height={"100%"} padding="10px 0" {...customStyles}>
-        <Box flex={isMobile || isTablet ? "none" : 1}>
-          <Image
-            src={PROFILE_URL}
-            alt="My Profile"
-            boxSize={isMobile ? 250 : isTablet ? 300 : 400}
-            borderRadius={8}
-            objectFit="cover"
-          />
-        </Box>
-        <Box flex={isMobile || isTablet ? "none" : 1}>
-          <Stack flexDirection="row" justifyContent="center">
-            <Box>1</Box>
-            <Box>2</Box>
+        <Stack
+          flex={1}
+          justifyContent={isMobile || isTablet ? "flex-start" : "center"}
+        >
+          <Stack gap={6} height={400} {...customStyles}>
+            <Box flex={isMobile || isTablet ? "none" : 1}>
+              <Image
+                src={PROFILE_URL}
+                alt="My Profile"
+                boxSize={isMobile ? 250 : isTablet ? 300 : 400}
+                borderRadius={8}
+                objectFit="cover"
+                maxH="100%"
+              />
+            </Box>
+
+            <Stack
+              flex={isMobile || isTablet ? "none" : 1}
+              justifyContent={
+                isMobile || isTablet ? "flex-start" : "space-between"
+              }
+              height="100%"
+            >
+              <Stack flexDirection="row" justifyContent="space-between">
+                <Box
+                  flex={1}
+                  borderRadius={4}
+                  border="1px"
+                  borderColor="default.text"
+                  height={100}
+                >
+                  <Stack
+                    alignItems="center"
+                    justifyContent="center"
+                    height="100%"
+                  >
+                    <PiMedal size={25} />
+                    <Text
+                      color="default.title"
+                      fontSize="sm"
+                      fontWeight="medium"
+                    >
+                      Experience
+                    </Text>
+                    <Text color="default.text" fontSize="xs">
+                      2+ Years
+                    </Text>
+                  </Stack>
+                </Box>
+                <Box
+                  flex={1}
+                  borderRadius={4}
+                  border="1px"
+                  borderColor="default.text"
+                  height={100}
+                  width={150}
+                >
+                  <Stack
+                    alignItems="center"
+                    justifyContent="center"
+                    height="100%"
+                  >
+                    <PiConfetti size={25} />
+                    <Text
+                      color="default.title"
+                      fontSize="sm"
+                      fontWeight="medium"
+                    >
+                      Completed
+                    </Text>
+                    <Text color="default.text" fontSize="xs">
+                      10+ Projects
+                    </Text>
+                  </Stack>
+                </Box>
+              </Stack>
+              <Text color="default.text" fontSize="md">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and
+                visual mockups.
+              </Text>
+              <Button height={50} width="50%">
+                <Text paddingRight={2}>Download CV</Text>{" "}
+                <CgFileDocument size={20} />
+              </Button>
+
+              {/* <Text color="default.text" fontSize="md">
+                Lorem ipsum is placeholder text commonly used in the graphic,
+                print, and publishing industries for previewing layouts and
+                visual mockups.
+              </Text> */}
+            </Stack>
           </Stack>
-          <Text color="default.text" fontSize="md">
-            Lorem ipsum is placeholder text commonly used in the graphic, print,
-            and publishing industries for previewing layouts and visual mockups.
-          </Text>
-        </Box>
+        </Stack>
       </Stack>
     </S.Wrapper>
   );
